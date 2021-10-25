48d5be10eeb1da3eb99c0cc449eb65c0193d49da
Glebasta33 <gleb.trusow@yandex.ru>
2021-10-25 14:41:00 +0300

    add rv & items layouts

--- app/src/main/java/com/example/shoppinglist/presentation/MainActivity.kt
+++ app/src/main/java/com/example/shoppinglist/presentation/MainActivity.kt
@@ -11,21 +11,13 @@ import com.example.shoppinglist.domain.ShopItem
 class MainActivity : AppCompatActivity() {
 
     private lateinit var viewModel: MainViewModel
-    private lateinit var tvTest: TextView
-    private var count = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        tvTest = findViewById(R.id.tvTest)
         viewModel = ViewModelProvider(this)[MainViewModel::class.java]
         viewModel.liveData.observe(this) {
-            tvTest.text = it.toString()
-            if (count == 0) {
-                count++
-                val item = it[2]
-                viewModel.changeEnableState(item)
-            }
+
         }
     }
 }
