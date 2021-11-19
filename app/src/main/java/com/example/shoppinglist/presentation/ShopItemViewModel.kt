d725786985d4e92fe86a938623eac0a8cdc9faeb
Glebasta33 <gleb.trusow@yandex.ru>
2021-11-19 13:10:13 +0300

    add suspend to method and scopes in view models

--- app/src/main/java/com/example/shoppinglist/presentation/ShopItemViewModel.kt
+++ app/src/main/java/com/example/shoppinglist/presentation/ShopItemViewModel.kt
@@ -1,16 +1,19 @@
 package com.example.shoppinglist.presentation
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
+import android.app.Application
+import androidx.lifecycle.*
 import com.example.shoppinglist.data.ShopListRepositoryImpl
 import com.example.shoppinglist.domain.AddShopItemUseCase
 import com.example.shoppinglist.domain.EditShopItemUseCase
 import com.example.shoppinglist.domain.GetShopItemUseCase
 import com.example.shoppinglist.domain.ShopItem
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.launch
 
-class ShopItemViewModel : ViewModel() {
-    private val repository = ShopListRepositoryImpl
+class ShopItemViewModel(application: Application) : AndroidViewModel(application) {
+    private val repository = ShopListRepositoryImpl(application)
     private val getShopItemUseCase = GetShopItemUseCase(repository)
     private val editShopItemUseCase = EditShopItemUseCase(repository)
     private val addShopItemUseCase = AddShopItemUseCase(repository)
@@ -32,32 +35,39 @@ class ShopItemViewModel : ViewModel() {
         get() = _activityIsReadyToClose
 
     fun getShopItem(shopItemId: Int) {
-        val item = getShopItemUseCase.getShopItem(shopItemId)
-        _shopItem.value = item
+        viewModelScope.launch {
+            val item = getShopItemUseCase.getShopItem(shopItemId)
+            _shopItem.value = item
+        }
     }
 
     fun addShopItem(inputName: String?, inputCount: String?) {
-        val name = parseName(inputName)
-        val count = parseCount(inputCount)
-        val isFieldsValid = validateInput(name, count)
-        if (isFieldsValid) {
-            val item = ShopItem(name, count, true)
-            addShopItemUseCase.addShopItem(item)
-            closeActivity()
+        viewModelScope.launch {
+            val name = parseName(inputName)
+            val count = parseCount(inputCount)
+            val isFieldsValid = validateInput(name, count)
+            if (isFieldsValid) {
+                val item = ShopItem(name, count, true)
+                addShopItemUseCase.addShopItem(item)
+                closeActivity()
+            }
         }
     }
 
     fun editShopItem(inputName: String?, inputCount: String?) {
-        val name = parseName(inputName)
-        val count = parseCount(inputCount)
-        val isFieldsValid = validateInput(name, count)
-        if (isFieldsValid) {
-            _shopItem.value?.let {
-                val item = it.copy(name = name, count = count)
-                editShopItemUseCase.editShopItem(item)
-                closeActivity()
+        viewModelScope.launch {
+            val name = parseName(inputName)
+            val count = parseCount(inputCount)
+            val isFieldsValid = validateInput(name, count)
+            if (isFieldsValid) {
+                _shopItem.value?.let {
+                    val item = it.copy(name = name, count = count)
+                    editShopItemUseCase.editShopItem(item)
+                    closeActivity()
+                }
             }
         }
+
     }
 
     private fun parseName(inputName: String?): String {
@@ -92,4 +102,5 @@ class ShopItemViewModel : ViewModel() {
     private fun closeActivity() {
         _activityIsReadyToClose.value = Unit
     }
+
 }
